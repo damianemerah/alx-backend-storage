69339a1d9f641e9ce8389b92caedc4db35f7ef3e
root <damianemerah1@gmail.com>
2023-07-13 11:13:38 +0000

    task 0

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,11 +1,9 @@
 -- id, integer, never null, auto increment and primary key
 -- email, string (255 characters), never null and unique
 -- name, string (255 characters)
-
 CREATE TABLE IF NOT EXISTS users (
   id INT AUTO_INCREMENT,
   email VARCHAR(255) NOT NULL,
   name VARCHAR(255),
   PRIMARY KEY (id)
 );
-
