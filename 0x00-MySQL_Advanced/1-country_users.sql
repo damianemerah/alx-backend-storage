5b6ccd9093a93e14e6ab46687fee38b73a28f585
root <damianemerah1@gmail.com>
2023-07-13 11:32:34 +0000

    task 1

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -5,8 +5,8 @@
 -- country, enumeration of countries: US, CO and TN, never null (= default will be the first element of the enumeration, here US)
 
 CREATE TABLE IF NOT EXISTS users (
-	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	email VARCHAR(255) NOT NULL UNIQUE,
-	name VARCHAR(255),
-	country NOT NULL ENUM ('US', 'CO', 'TN') DEFAULT 'US'
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  name VARCHAR(255),
+  country NOT NULL ENUM('US', 'CO', 'TN') DEFAULT 'US'
 );
