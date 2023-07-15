817b1369f5eaaa8defc236672b2be8f801c3ccfa
Damian <damianemerah1@gmail.com>
2023-07-15 17:11:44 +0100

    task 8-10

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,11 @@
+-- creates a function SafeDiv that divides (and returns) the first by the second number or returns 0 if the second number is equal to 0.
+
+DELIMITER //
+
+CREATE FUNCTION SafeDiv(IN a INT, IN b INT)
+RETURNS FLOAT
+BEGIN
+    RETURN IF(b = 0, 0, a / b);
+END //
+
+DELIMITER ;
--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,27 +1,23 @@
--- a SQL script that creates a stored procedure ComputeAverageScoreForUser that computes and store the average score for a student. Note: An average score can be a decimal
+-- Creates a stored procedure ComputeAverageScoreForUser that
+-- computes and stores the average score for a student.
 DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
-
--- Create the stored procedure
-DELIMITER //
-
-CREATE PROCEDURE ComputeAverageScoreForUser(
-    IN user_id INT
-)
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageScoreForUser (user_id INT)
 BEGIN
-    DECLARE total_score FLOAT;
-    DECLARE projects_count INT;
+    DECLARE total_score INT DEFAULT 0;
+    DECLARE projects_count INT DEFAULT 0;
 
-    -- Compute the total score and count of projects for the user
-    SELECT SUM(score), COUNT(*) INTO total_score, projects_count
-    FROM corrections
-    WHERE user_id = user_id;
+    SELECT SUM(score)
+        INTO total_score
+        FROM corrections
+        WHERE corrections.user_id = user_id;
+    SELECT COUNT(*)
+        INTO projects_count
+        FROM corrections
+        WHERE corrections.user_id = user_id;
 
-    -- Compute and store the average score for the user
     UPDATE users
-    SET average_score = IF(projects_count = 0, 0, total_score / projects_count)
-    WHERE id = user_id;
-
-    SELECT "Average score computed and stored.";
-END //
-
+        SET users.average_score = IF(projects_count = 0, 0, total_score / projects_count)
+        WHERE users.id = user_id;
+END $$
 DELIMITER ;
